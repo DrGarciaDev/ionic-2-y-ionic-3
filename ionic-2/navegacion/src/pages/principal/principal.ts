cbd979b563edce9737e405c2845b3339d1e44762
Luis García <luis_grdz@hotmail.com>
2019-08-07 22:18:07 -0500

    Avance hasta el video 45, se creó la aplicación 'navegación' con diferentes páginas creadas por comando, se cambia la pagina home por la principal como página root, y se crean botones para navegar con evento click y evento navpush

---
+++ ionic-2/navegacion/src/pages/principal/principal.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+import { Pagina2Page } from '../pagina2/pagina2';
+
+/**
+ * Generated class for the PrincipalPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ * @author Luis Alberto García Rodríguez
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-principal',
+  templateUrl: 'principal.html',
+})
+export class PrincipalPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  // Crea una variable de tipo any
+  pagina2:any = Pagina2Page;
+
+  public navegar_pagina(){
+    this.navCtrl.push( Pagina2Page );
+  }
+
+}
