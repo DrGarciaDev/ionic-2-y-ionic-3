685464b924301dd6f2b59cb8ccb785cbfecf6b3d
Luis García <luis_grdz@hotmail.com>
2019-08-08 23:02:17 -0500

    Video 48: Crear modal y enviar y recibir parámetros en ellos

--- ionic-2/navegacion/src/pages/modal/modal.ts
+++ ionic-2/navegacion/src/pages/modal/modal.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, ViewController, NavParams } from 'ionic-angular';
 
 /**
  * Generated class for the ModalPage page.
  *
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
+ * @author Luis Alberto García Rodríguez
  */
 
 @IonicPage()
@@ -15,11 +16,32 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ModalPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  nombre:string = "";
+  edad:number = 0;
+
+  constructor(public viewCtrl: ViewController, public navParams: NavParams) {
+    this.nombre = this.navParams.get("nombre");
+    this.edad = this.navParams.get("edad");
+
+    console.log( this.nombre, this.edad );
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ModalPage');
+  cerrar_modal_con_parametros(){
+    //Objeto para enviar com parametros
+    let data = {
+      nombre: "Juan",
+      edad: 18,
+      coords: {
+        lat: 10,
+        lng: -10
+      }
+    };
+
+    this.viewCtrl.dismiss( data );
+  }
+
+  cerrar_modal_sin_parametros(){
+    this.viewCtrl.dismiss();
   }
 
 }
